159998c806a4d761b3f91c663c85151878f3e6df
Rivka-Fogelman <76700130+Rivka-Fogelman@users.noreply.github.com>
2021-09-29 23:12:19 +0300

    update login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -4,11 +4,12 @@ import { Button, Form } from 'semantic-ui-react'
 import './login.css'
 function Login(props) {
     const [isLoading, setIsLoading]=useState(false)
+    const [id, setId]=useState(null)
        return (<>
     <Form>
     <Form.Field>
       <label>Please Enter Your ID</label>
-      <input placeholder='ID' type="number"></input>
+      <input placeholder='ID' type="number" value={id} onChange={(e)=>{setId(e.target.value)}}></input>
     </Form.Field>
     {isLoading?
         <Form loading></Form>:
@@ -16,7 +17,7 @@ function Login(props) {
     }
     <Button type='submit' onClick={()=>{props.setIsCorrectId(true); setIsLoading(true)}}>Submit</Button>
     
-    {!props.isCorrectId?
+    {!props.isCorrectId && id ? 
     <label className="notExistID">Your ID does not exist in the system</label>
     :<span/>
     }
