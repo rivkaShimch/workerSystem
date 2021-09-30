a8071b6a76cbe9cde8b560326603a4aea3b66803
Rivka-Fogelman <76700130+Rivka-Fogelman@users.noreply.github.com>
2021-09-30 14:41:53 +0300

    add services file

---
+++ services/user.services.js
@@ -0,0 +1,20 @@
+getUser = (id) => {
+    return fetch("https://goofy-ride-8664d8.netlify.app/.netlify/functions/api/getUser",
+        {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id": id
+                })
+        .then((res) => {
+            return res.json();
+        })
+        .then((result) => {
+            console.log("user from getUser", result.user)
+            return result.user
+        }, (err) => {
+        })
+}
+export {getUser}
